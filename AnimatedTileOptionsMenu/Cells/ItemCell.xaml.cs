4bb6401127c5d1913c0799bf262e8589542e2b67
NordAlex <alexhac94@gmail.com>
2019-11-17 02:18:59 +0200

    Add feed cell

---
+++ AnimatedTileOptionsMenu/Cells/ItemCell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimatedTileOptionsMenu.Cells
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemCell : ViewCell
+    {
+        public ItemCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
